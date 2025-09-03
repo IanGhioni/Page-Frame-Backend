e0f11669e3acbb687420b4c290909a2e811fa56c
IanGhioni <ghioni2002@gmail.com>
2025-09-03 15:08:06 -0300

    Añadido cross origin.

--- src/main/java/ar/edu/unq/spring/controller/ContenidoControllerREST.java
+++ src/main/java/ar/edu/unq/spring/controller/ContenidoControllerREST.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/contenido")
+@CrossOrigin("http://localhost:5173")
 public class ContenidoControllerREST {
     public final ContenidoService contenidoService;
 
