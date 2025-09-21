0ed555a842c1f148037ab9091464b7a29c72e060
Candela <candelamarzaroli@gmail.com>
2025-09-21 16:09:56 -0300

    fotos de perfil

--- src/main/java/ar/edu/unq/spring/controller/dto/UsuarioResponseDTO.java
+++ src/main/java/ar/edu/unq/spring/controller/dto/UsuarioResponseDTO.java
@@ -1,11 +1,13 @@
 package ar.edu.unq.spring.controller.dto;
 
+import ar.edu.unq.spring.modelo.Usuario;
+
 import java.util.List;
 
-public record UsuarioResponseDTO (Long id, String username, String email, List<ContenidoDeUsuarioSimpleResponseDTO> contenidos) {
-    public static UsuarioResponseDTO fromModel(ar.edu.unq.spring.modelo.Usuario usuario) {
+public record UsuarioResponseDTO(Long id, String username, String email, List<ContenidoDeUsuarioSimpleResponseDTO> contenidos, String fotoPerfil) {
+    public static UsuarioResponseDTO fromModel(Usuario usuario) {
         return new UsuarioResponseDTO(usuario.getId(), usuario.getUsername(), usuario.getEmail(), usuario.getMisContenidos().stream()
                 .map(ContenidoDeUsuarioSimpleResponseDTO::fromModel)
-                .toList());
+                .toList(), usuario.getFotoPerfil());
     }
 }
