b9b857f3e46055afe810c21ba98804c52d9950c8
ornellaBottiggi <bottiggiornella@gmail.com>
2025-09-27 17:11:21 -0300

    bugs arreglados

--- src/main/java/ar/edu/unq/spring/controller/dto/UsuarioResponseDTO.java
+++ src/main/java/ar/edu/unq/spring/controller/dto/UsuarioResponseDTO.java
@@ -1,13 +1,15 @@
 package ar.edu.unq.spring.controller.dto;
 
+import ar.edu.unq.spring.modelo.ContenidoDeUsuario;
 import ar.edu.unq.spring.modelo.Usuario;
 
 import java.util.List;
+import java.util.Set;
 
-public record UsuarioResponseDTO(Long id, String username, String email, List<ContenidoDeUsuarioSimpleResponseDTO> contenidos, String fotoPerfil) {
+public record UsuarioResponseDTO(Long id, String username, String email, List<ContenidoDeUsuarioSimpleResponseDTO> contenidos, List<ContenidoDeUsuarioPersonalizadoSimpleResponseDTO> contenidoPersonalizado, String fotoPerfil) {
     public static UsuarioResponseDTO fromModel(Usuario usuario) {
         return new UsuarioResponseDTO(usuario.getId(), usuario.getUsername(), usuario.getEmail(), usuario.getMisContenidos().stream()
                 .map(ContenidoDeUsuarioSimpleResponseDTO::fromModel)
-                .toList(), usuario.getFotoPerfil());
+                .toList(), usuario.getListasPersonalizadas().stream().map(ContenidoDeUsuarioPersonalizadoSimpleResponseDTO::fromModel).toList() ,usuario.getFotoPerfil());
     }
 }
