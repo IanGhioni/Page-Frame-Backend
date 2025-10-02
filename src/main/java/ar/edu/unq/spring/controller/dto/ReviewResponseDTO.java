2c85df8954e8133910d4b0caa2e3ebc4c09e7ae2
Candela <candelamarzaroli@gmail.com>
2025-10-01 23:05:11 -0300

    arreglos en dtos

--- src/main/java/ar/edu/unq/spring/controller/dto/ReviewResponseDTO.java
+++ src/main/java/ar/edu/unq/spring/controller/dto/ReviewResponseDTO.java
@@ -2,8 +2,8 @@ package ar.edu.unq.spring.controller.dto;
 
 import ar.edu.unq.spring.modelo.Review;
 
-public record ReviewResponseDTO(Long id, Long usuarioId, Long contenidoId, Double valoracion) {
+public record ReviewResponseDTO(Long id, Long usuarioId, Long contenidoId, Double valoracion, String texto) {
     public static ReviewResponseDTO fromModel(Review review) {
-        return new ReviewResponseDTO( review.getId(), review.getUsuario().getId(), review.getContenido().getId(),  review.getValoracion() );
+        return new ReviewResponseDTO( review.getId(), review.getUsuario().getId(), review.getContenido().getId(),  review.getValoracion(), review.getTexto());
     }
 }
